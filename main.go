5b1b2c9573b5b366bc76c5a1febb69af6353fff6
Jurijs Klopovskis <jurijs.klopovskis@zabbix.com>
2023-01-23 20:49:32 +0200

    .......... [ZBXNEXT-826] updated plugin version to 6.4.0beta6

--- main.go
+++ main.go
@@ -28,9 +28,9 @@ import (
 )
 
 const PLUGIN_VERSION_MAJOR = 6
-const PLUGIN_VERSION_MINOR = 0
-const PLUGIN_VERSION_PATCH = 13
-const PLUGIN_VERSION_RC    = "rc1"
+const PLUGIN_VERSION_MINOR = 4
+const PLUGIN_VERSION_PATCH = 0
+const PLUGIN_VERSION_RC    = "beta6"
 
 func main() {
 	handleFlags()
