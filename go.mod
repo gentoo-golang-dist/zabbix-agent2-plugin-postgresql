7cdffaba68071c4bc1ab2652dd4c7447ca617a7d
Mihails Prihodko <mihails.prihodko@zabbix.com>
2023-04-05 11:08:14 +0300

    ...G...... [ZBX-22532] created custom help message in postgresql plugin

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module git.zabbix.com/ap/postgresql
 go 1.18
 
 require (
-	git.zabbix.com/ap/plugin-support v1.2.2-0.20230123112943-76ee71718c5e
-	github.com/jackc/pgx/v4 v4.17.2
+	git.zabbix.com/ap/plugin-support v1.2.2-0.20230405081140-5ac243dd55a6
+	github.com/jackc/pgx/v4 v4.12.1-0.20210724153913-640aa07df17c
 	github.com/omeid/go-yarn v0.0.1
 )
 
--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 /*
 ** Zabbix
-** Copyright 2001-2022 Zabbix SIA
+** Copyright 2001-2023 Zabbix SIA
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 
 	"git.zabbix.com/ap/plugin-support/plugin/comms"
 	"git.zabbix.com/ap/plugin-support/plugin/container"
@@ -30,7 +29,8 @@ import (
 const PLUGIN_VERSION_MAJOR = 6
 const PLUGIN_VERSION_MINOR = 0
 const PLUGIN_VERSION_PATCH = 15
-const PLUGIN_VERSION_RC    = ""
+const PLUGIN_VERSION_RC = ""
+const PLUGIN_LICENSE_YEAR = 2023
 
 func main() {
 	handleFlags()
@@ -48,48 +48,23 @@ func main() {
 }
 
 func handleFlags() {
-	var versionFlag bool
-	const (
-		versionDefault     = false
-		versionDescription = "Print program version and exit"
-	)
-	flag.BoolVar(&versionFlag, "version", versionDefault, versionDescription)
-	flag.BoolVar(&versionFlag, "V", versionDefault, versionDescription+" (shorthand)")
-
-	var helpFlag bool
-	const (
-		helpDefault     = false
-		helpDescription = "Display this help message"
-	)
-	flag.BoolVar(&helpFlag, "help", helpDefault, helpDescription)
-	flag.BoolVar(&helpFlag, "h", helpDefault, helpDescription+" (shorthand)")
+	comms.InitCommonFlags()
 
 	flag.Parse()
 
-	if helpFlag || len(os.Args) == 1 {
-		flag.Usage()
-		os.Exit(0)
-	}
-
-	flag.Visit(func(f *flag.Flag) {
-		switch f.Name {
-		case "V", "version":
-			comms.PrintVersion(plugin.Name, copyrightMessage(),
-				PLUGIN_VERSION_MAJOR, PLUGIN_VERSION_MINOR, PLUGIN_VERSION_PATCH, PLUGIN_VERSION_RC)
-			os.Exit(0)
-		}
-	})
+	comms.VisitCommonFlags(plugin.Name, copyrightMessage(), comms.GetCommonHelpMessage(), PLUGIN_VERSION_MAJOR,
+		PLUGIN_VERSION_MINOR, PLUGIN_VERSION_PATCH, PLUGIN_VERSION_RC)
 }
 
 func copyrightMessage() string {
-	return  "Copyright 2001-2022 Zabbix SIA\n" +
-		"Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-		"you may not use this file except in compliance with the License.\n" +
-		"You may obtain a copy of the License at\n\n" +
-		"\thttp://www.apache.org/licenses/LICENSE-2.0\n\n" +
-		"Unless required by applicable law or agreed to in writing, software\n" +
-		"distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-		"See the License for the specific language governing permissions and\n" +
-		"limitations under the License.\n"
+	return fmt.Sprintf("Copyright 2001-%d Zabbix SIA\n"+
+		"Licensed under the Apache License, Version 2.0 (the \"License\");\n"+
+		"you may not use this file except in compliance with the License.\n"+
+		"You may obtain a copy of the License at\n\n"+
+		"\thttp://www.apache.org/licenses/LICENSE-2.0\n\n"+
+		"Unless required by applicable law or agreed to in writing, software\n"+
+		"distributed under the License is distributed on an \"AS IS\" BASIS,\n"+
+		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"+
+		"See the License for the specific language governing permissions and\n"+
+		"limitations under the License.\n", PLUGIN_LICENSE_YEAR)
 }
