c23372821314f4caf9e2f1e3df45c217248d1bef
Mihails Prihodko <mihails.prihodko@zabbix.com>
2023-08-17 12:25:39 +0300

    [ZBX-22532] created custom help message

--- main.go
+++ main.go
@@ -18,9 +18,7 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"os"
 
 	"git.zabbix.com/ap/plugin-support/plugin/comms"
 	"git.zabbix.com/ap/plugin-support/plugin/container"
@@ -30,10 +28,12 @@ import (
 const PLUGIN_VERSION_MAJOR = 7
 const PLUGIN_VERSION_MINOR = 0
 const PLUGIN_VERSION_PATCH = 0
-const PLUGIN_VERSION_RC    = "alpha3"
+const PLUGIN_VERSION_RC = "alpha3"
+const PLUGIN_LICENSE_YEAR = 2023
 
 func main() {
-	handleFlags()
+	comms.HandleCommonFlags(plugin.Name, copyrightMessage(), PLUGIN_VERSION_MAJOR, PLUGIN_VERSION_MINOR,
+		PLUGIN_VERSION_PATCH, PLUGIN_VERSION_RC)
 
 	h, err := container.NewHandler(plugin.Impl.Name())
 	if err != nil {
@@ -47,49 +47,15 @@ func main() {
 	}
 }
 
-func handleFlags() {
-	var versionFlag bool
-	const (
-		versionDefault     = false
-		versionDescription = "Print program version and exit"
-	)
-	flag.BoolVar(&versionFlag, "version", versionDefault, versionDescription)
-	flag.BoolVar(&versionFlag, "V", versionDefault, versionDescription+" (shorthand)")
-
-	var helpFlag bool
-	const (
-		helpDefault     = false
-		helpDescription = "Display this help message"
-	)
-	flag.BoolVar(&helpFlag, "help", helpDefault, helpDescription)
-	flag.BoolVar(&helpFlag, "h", helpDefault, helpDescription+" (shorthand)")
-
-	flag.Parse()
-
-	if helpFlag || len(os.Args) == 1 {
-		flag.Usage()
-		os.Exit(0)
-	}
-
-	flag.Visit(func(f *flag.Flag) {
-		switch f.Name {
-		case "V", "version":
-			comms.PrintVersion(plugin.Name, copyrightMessage(),
-				PLUGIN_VERSION_MAJOR, PLUGIN_VERSION_MINOR, PLUGIN_VERSION_PATCH, PLUGIN_VERSION_RC)
-			os.Exit(0)
-		}
-	})
-}
-
 func copyrightMessage() string {
-	return  "Copyright 2001-2023 Zabbix SIA\n" +
-		"Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-		"you may not use this file except in compliance with the License.\n" +
-		"You may obtain a copy of the License at\n\n" +
-		"\thttp://www.apache.org/licenses/LICENSE-2.0\n\n" +
-		"Unless required by applicable law or agreed to in writing, software\n" +
-		"distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-		"See the License for the specific language governing permissions and\n" +
-		"limitations under the License.\n"
+	return fmt.Sprintf("Copyright 2001-%d Zabbix SIA\n"+
+		"Licensed under the Apache License, Version 2.0 (the \"License\");\n"+
+		"you may not use this file except in compliance with the License.\n"+
+		"You may obtain a copy of the License at\n\n"+
+		"\thttp://www.apache.org/licenses/LICENSE-2.0\n\n"+
+		"Unless required by applicable law or agreed to in writing, software\n"+
+		"distributed under the License is distributed on an \"AS IS\" BASIS,\n"+
+		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"+
+		"See the License for the specific language governing permissions and\n"+
+		"limitations under the License.\n", PLUGIN_LICENSE_YEAR)
 }
