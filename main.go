59c2dde8f8a5b191778b3c729b846096629981c4
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2023-08-29 16:01:34 +0300

    [ZBXNEXT-826] updated release version to 6.0.22rc1

--- main.go
+++ main.go
@@ -29,8 +29,8 @@ import (
 
 const PLUGIN_VERSION_MAJOR = 6
 const PLUGIN_VERSION_MINOR = 0
-const PLUGIN_VERSION_PATCH = 21
-const PLUGIN_VERSION_RC    = ""
+const PLUGIN_VERSION_PATCH = 22
+const PLUGIN_VERSION_RC    = "rc1"
 
 func main() {
 	handleFlags()
