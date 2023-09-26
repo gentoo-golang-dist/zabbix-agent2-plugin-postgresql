2523913c8351e9409c9da7b7dc79b7fd036c329e
Continuous Integration <ci@zabbix.com>
2023-09-26 11:25:45 +0300

    [ZBXNEXT-826] updated release version for 6.4.7

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 const PLUGIN_VERSION_MAJOR = 6
 const PLUGIN_VERSION_MINOR = 4
 const PLUGIN_VERSION_PATCH = 7
-const PLUGIN_VERSION_RC    = "rc1"
+const PLUGIN_VERSION_RC    = ""
 
 func main() {
 	handleFlags()
