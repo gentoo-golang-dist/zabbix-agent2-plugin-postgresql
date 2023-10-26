cc2cb98ed9415945d5bf75194b136d2816ebc41a
Natalja Romancaka <natalja.romancaka@zabbix.com>
2023-10-26 10:57:21 +0300

    [ZBXNEXT-826] reverted version changes for 6.0.23rc2

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 const PLUGIN_VERSION_MAJOR = 6
 const PLUGIN_VERSION_MINOR = 0
 const PLUGIN_VERSION_PATCH = 23
-const PLUGIN_VERSION_RC    = "rc2"
+const PLUGIN_VERSION_RC    = "rc1"
 
 func main() {
 	handleFlags()
