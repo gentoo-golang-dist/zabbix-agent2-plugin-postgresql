4644ee958bdd41ee8c9fc71caa8c04b4b0552df2
Natalja Romancaka <natalja.romancaka@zabbix.com>
2023-11-01 11:42:27 +0200

    [ZBXNEXT-826] updated release version to 6.4.9rc1

--- main.go
+++ main.go
@@ -29,8 +29,8 @@ import (
 
 const PLUGIN_VERSION_MAJOR = 6
 const PLUGIN_VERSION_MINOR = 4
-const PLUGIN_VERSION_PATCH = 8
-const PLUGIN_VERSION_RC    = ""
+const PLUGIN_VERSION_PATCH = 9
+const PLUGIN_VERSION_RC    = "rc1"
 
 func main() {
 	handleFlags()
