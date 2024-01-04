8707b37f1c94b5c91ee3c448233f1f09dd6ae7fd
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2024-01-04 06:41:27 -0100

    .......... [ZBXNEXT-826] updated copyright year [2024]

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 /*
 ** Zabbix
-** Copyright 2001-2023 Zabbix SIA
+** Copyright 2001-2024 Zabbix SIA
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ import (
 )
 
 const COPYRIGHT_MESSAGE = //
-`Copyright 2001-2023 Zabbix SIA
+`Copyright 2001-2024 Zabbix SIA
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
