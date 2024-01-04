a8e2840225ee9b01faabc4f37444f74f3bddf62c
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2024-01-04 06:40:29 -0100

    .......... [ZBXNEXT-826] updated copyright year [2024]

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 /*
 ** Zabbix
-** Copyright 2001-2023 Zabbix SIA
+** Copyright 2001-2024 Zabbix SIA
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -82,7 +82,7 @@ func handleFlags() {
 }
 
 func copyrightMessage() string {
-	return  "Copyright 2001-2023 Zabbix SIA\n" +
+	return  "Copyright 2001-2024 Zabbix SIA\n" +
 		"Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
 		"you may not use this file except in compliance with the License.\n" +
 		"You may obtain a copy of the License at\n\n" +
