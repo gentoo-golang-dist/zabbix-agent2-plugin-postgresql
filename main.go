bb2e926b2c5c16ef5315354713ca60d8f1587585
Continuous Integration <ci@zabbix.com>
2024-06-17 12:30:26 +0300

    .......... [ZBXNEXT-826] updated release version for 6.4.16

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ import (
 const PLUGIN_VERSION_MAJOR = 6
 const PLUGIN_VERSION_MINOR = 4
 const PLUGIN_VERSION_PATCH = 16
-const PLUGIN_VERSION_RC = "rc2"
+const PLUGIN_VERSION_RC = ""
 
 const COPYRIGHT_MESSAGE = //
 `Copyright 2001-2024 Zabbix SIA
