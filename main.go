a0d082172a097b9d28bab147b0a6278bb7f84d07
Stanislavs Jurgensons <stanislavs.jurgensons@zabbix.com>
2024-08-12 15:58:27 +0300

    ...G...... [DEV-3904] fixed errors caused by plugin/container package refactoring

--- main.go
+++ main.go
@@ -70,7 +70,8 @@ func main() {
 	if err != nil {
 		panic(fmt.Sprintf("failed to create plugin handler %s", err.Error()))
 	}
-	plugin.Impl.Logger = &h
+
+	plugin.Impl.Logger = h
 
 	err = h.Execute()
 	if err != nil {
