5d87fae7288151507a530976aaf402855e56c06a
Eriks Sneiders <eriks.sneiders@zabbix.com>
2025-02-11 14:22:08 +0200

    ...G...... [DEV-3542] added nolint

--- plugin/config.go
+++ plugin/config.go
@@ -55,7 +55,7 @@ type Session struct {
 
 // PluginOptions are options for PostgreSQL connection.
 type PluginOptions struct {
-	System plugin.SystemOptions `conf:"optional"`
+	System plugin.SystemOptions `conf:"optional"` //nolint:staticcheck
 	// Timeout is the maximum time in seconds for waiting when a connection has to be established.
 	// Default value equals to the global agent timeout.
 	Timeout int `conf:"optional,range=1:30"`
