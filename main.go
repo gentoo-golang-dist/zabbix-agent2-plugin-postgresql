6dce142015c58cdb3d02d1160c83968527f53f20
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2025-04-22 17:08:51 +0300

    .......... [ZBXNEXT-826] updated release version to 7.0.13rc1

--- main.go
+++ main.go
@@ -34,8 +34,8 @@ the license. There is NO WARRANTY, to the extent permitted by law.`
 const (
 	PLUGIN_VERSION_MAJOR = 7
 	PLUGIN_VERSION_MINOR = 0
-	PLUGIN_VERSION_PATCH = 12
-	PLUGIN_VERSION_RC    = ""
+	PLUGIN_VERSION_PATCH = 13
+	PLUGIN_VERSION_RC    = "rc1"
 )
 
 func main() {
