f419fd19672fd067e729eb791a1d1e31dd616b77
Natalja Romancaka <natalja.romancaka@zabbix.com>
2025-07-03 09:57:59 +0300

    .......... [ZBXNEXT-826] updated release version to 7.4.1rc1

--- main.go
+++ main.go
@@ -34,8 +34,8 @@ the license. There is NO WARRANTY, to the extent permitted by law.`
 const (
 	PLUGIN_VERSION_MAJOR = 7
 	PLUGIN_VERSION_MINOR = 4
-	PLUGIN_VERSION_PATCH = 0
-	PLUGIN_VERSION_RC    = ""
+	PLUGIN_VERSION_PATCH = 1
+	PLUGIN_VERSION_RC    = "rc1"
 )
 
 func main() {
