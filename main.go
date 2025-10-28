8b553c2df713d0312ff290406ebfa94f6eb2936d
Continuous Integration <ci@zabbix.com>
2025-10-28 11:44:49 +0200

    .......... [ZBXNEXT-826] updated release version for 7.2.14

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ const (
 	PLUGIN_VERSION_MAJOR = 7
 	PLUGIN_VERSION_MINOR = 2
 	PLUGIN_VERSION_PATCH = 14     //nolint:revive
-	PLUGIN_VERSION_RC    = "rc1" //nolint:revive
+	PLUGIN_VERSION_RC    = "" //nolint:revive
 )
 
 func main() {
