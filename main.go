edddf54740dbe6628068ff54d3e6d10acc3dcfc9
Kirils Seve <kirils.seve@zabbix.com>
2026-07-10 09:55:53 +0300

    .......... [ZBXNEXT-826] updated release version to 7.0.29rc1

--- main.go
+++ main.go
@@ -34,8 +34,8 @@ the license. There is NO WARRANTY, to the extent permitted by law.`
 const (
 	PLUGIN_VERSION_MAJOR = 7
 	PLUGIN_VERSION_MINOR = 0
-	PLUGIN_VERSION_PATCH = 28    //nolint:revive
-	PLUGIN_VERSION_RC    = "" //nolint:revive
+	PLUGIN_VERSION_PATCH = 29    //nolint:revive
+	PLUGIN_VERSION_RC    = "rc1" //nolint:revive
 )
 
 func main() {
